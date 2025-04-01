c5e469883cb81e0fd3683fbb5d503f5321d4f6a2
briana-rice <1brianarice@gmail.com>
2025-03-31 19:08:57 -0500

    Update codebuild-role.tf

--- ci-cd/codebuild-role.tf
+++ ci-cd/codebuild-role.tf
@@ -1,1 +1,21 @@
 # This file defines the IAM role and policy for AWS CodeBuild to access
+resource "aws_iam_role" "codebuild_service_role" {
+  name = "CodeBuildServiceRole"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "codebuild_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+# Attach a managed policy or inline policy that grants minimal permissions
+resource "aws_iam_role_policy_attachment" "codebuild_service_role_attach" {
+  role       = aws_iam_role.codebuild_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
